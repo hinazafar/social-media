574781ca51ac267443f5ab755dddeaa50728cae7
Hina Zafar <hina.nida@gmail.com>
2024-06-21 21:19:52 +0500

    use Effect hook and signal controller usage

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,27 +1,35 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
 
-  const handleGetPostsClick = () => {
-    fetch("https://dummyjson.com/posts")
+  const [fetching, setFetching] = useState(false);
+
+  useEffect(() => {
+    setFetching(true);
+    const controller = new AbortController();
+    const signal = controller.signal;
+    fetch("https://dummyjson.com/posts", { signal })
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts);
+        setFetching(false);
       });
-  };
+    return () => {
+      console.log("Cleaning up Effect");
+      controller.abort();
+    };
+  }, []);
 
   return (
     <>
-      {postList.length === 0 && (
-        <WelcomeMessage onGetPostsClick={handleGetPostsClick} />
-      )}
-      {postList.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      {fetching && <LoadingSpinner />}
+      {!fetching && postList.length === 0 && <WelcomeMessage />}
+      {!fetching && postList.map((post) => <Post key={post.id} post={post} />)}
     </>
   );
 };
--- src/components/WelcomeMessage.jsx
+++ src/components/WelcomeMessage.jsx
@@ -1,14 +1,7 @@
-const WelcomeMessage = ({ onGetPostsClick }) => {
+const WelcomeMessage = () => {
   return (
     <center className="welcome-message">
       <h1>There are no Posts!</h1>
-      <button
-        className="btn btn-primary"
-        type="button"
-        onClick={onGetPostsClick}
-      >
-        Get Posts from Server
-      </button>
     </center>
   );
 };
